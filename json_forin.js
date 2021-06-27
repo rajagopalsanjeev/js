62045eb69f8c3bf49e339725418f7ee20e011c79
rajagopalsanjeev <86050033+rajagopalsanjeev@users.noreply.github.com>
2021-06-27 19:02:31 +0530

    Add files via upload

---
+++ json_forin.js
@@ -0,0 +1,8 @@
+var obj = [
+          { person: "Name 1", age: "2", company: "GUVI" },
+          { person: "Name 2", age: "5", company: "GUVI geek" },
+          { person: "Name 3", age: "8", company: "GUVI geek network" },
+        ]
+        for(var k in obj){
+        console.log(obj[k]);
+        }
